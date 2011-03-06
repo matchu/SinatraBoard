895f34b55d9c73fd28fba81e1659315faaaf778b
Matchu <matchu1993@gmail.com>
2011-03-05 23:04:37 -0500

    explain the resource API

--- panes/reddit/user.rb
+++ panes/reddit/user.rb
@@ -1,13 +1,20 @@
 class Reddit
   class User < Resource
-    format :json
-    base_uri "www.reddit.com/user"
+    format :json                       # It's a JSON resource
+    base_uri "www.reddit.com/users"    # that lives in www.reddit.com/users,
+    source { "/#{@name}/about.json" }  # specifically, at /[name]/about.json,
+    scope 'data'                       # and the data we're interested in is
+                                       # all under the 'data' key
 
-    source { "/#{@name}/about.json" }
-    scope 'data'
+    # Compare this file to twitter/user.rb and you'll notice that the #source
+    # method can take both a plain string and a string in a block. When the
+    # result depends on the pane's specific configuration, use a block. When
+    # it would be the same for any pane of this type, use a string.
 
+    # We'll be exposing the #comment_karma and #link_karma JSON keys
     attr_resource :comment_karma, :link_karma
 
+    # Now, here's some custom behavior regarding how a user gets its name
     attr_reader :name
 
     def initialize(name)
--- panes/twitter/user.rb
+++ panes/twitter/user.rb
@@ -1,13 +1,14 @@
 class Twitter
   class User < Resource
-    format :json
-    base_uri "http://api.twitter.com/1"
+    format :json                         # It's a JSON resource
+    base_uri "api.twitter.com/1"         # that lives in api.twitter.com/1,
+    source "/users/show.json"            # specifically, at /users/show.json
+    query { {:screen_name => @name} }    # when we provide ?screen_name=[name]
 
-    source "/users/show.json"
-    query { {:screen_name => @name} }
-
+    # We'll be exposing the #followers_count JSON key
     attr_resource :followers_count
 
+    # Now, here's some custom behavior about how a user gets its name
     attr_reader :name
 
     def initialize(name)
